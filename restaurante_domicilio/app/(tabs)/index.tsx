c824e094d3c2fb32a066fd9b8841d95f00b6599e
JuanFeMartinez <jfmartinezv@uniboyaca.edu.co>
2024-11-03 21:16:26 -0500

    imagenes añadidas, no me funciona en expo go la aplicacion pero en navegador funciona perfectamente

--- restaurante_domicilio/app/(tabs)/index.tsx
+++ restaurante_domicilio/app/(tabs)/index.tsx
@@ -1,26 +1,25 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet, ScrollView, Button, TextInput, Alert, TouchableOpacity, Image } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome'; // Importamos el ícono de FontAwesome
-const imgT = require('../../assets/images/limonada.png')
 
 const menuItems = [
-  { nombre: 'Limonada', tipo: 'Bebidas Frías', descripcion: 'Limonada fresca con hielo', precio: 5000 },
-  { nombre: 'Coca-Cola', tipo: 'Bebidas Frías', descripcion: 'Refresco de cola', precio: 4500 },
-  { nombre: 'Té Helado', tipo: 'Bebidas Frías', descripcion: 'Té con limón y hielo', precio: 6000 },
-  { nombre: 'Agua Mineral', tipo: 'Bebidas Frías', descripcion: 'Agua con gas', precio: 3500 },
-  { nombre: 'Sopa de Pollo', tipo: 'Sopas', descripcion: 'Sopa casera de pollo con fideos', precio: 12000 },
-  { nombre: 'Sopa de Tomate', tipo: 'Sopas', descripcion: 'Sopa cremosa de tomate', precio: 11000 },
-  { nombre: 'Sopa de Verduras', tipo: 'Sopas', descripcion: 'Sopa mixta de verduras', precio: 11500 },
-  { nombre: 'Sopa de Mariscos', tipo: 'Sopas', descripcion: 'Sopa con una mezcla de mariscos', precio: 15000 },
-  { nombre: 'Pollo a la Parrilla', tipo: 'Platos del Día', descripcion: 'Pollo con arroz y ensalada', precio: 20000 },
-  { nombre: 'Carne Asada', tipo: 'Platos del Día', descripcion: 'Carne de res con papas fritas', precio: 22000 },
-  { nombre: 'Pasta Alfredo', tipo: 'Platos a la Carta', descripcion: 'Pasta con salsa alfredo y pollo', precio: 18000 },
-  { nombre: 'Pizza Margarita', tipo: 'Platos a la Carta', descripcion: 'Pizza con tomate, mozzarella y albahaca', precio: 25000 },
-  { nombre: 'Hamburguesa', tipo: 'Platos a la Carta', descripcion: 'Hamburguesa con queso y papas fritas', precio: 17000 },
-  { nombre: 'Tacos', tipo: 'Platos a la Carta', descripcion: 'Tacos con carne, queso y guacamole', precio: 16000 },
-  { nombre: 'Ensalada César', tipo: 'Platos a la Carta', descripcion: 'Ensalada con pollo y aderezo César', precio: 14000 },
-  { nombre: 'Risotto de Champiñones', tipo: 'Platos a la Carta', descripcion: 'Risotto cremoso con champiñones', precio: 20000 },
-  { nombre: 'Menú Infantil', tipo: 'Menú Infantil', descripcion: 'Nuggets de pollo con papas fritas', precio: 12000 },
+  { nombre: 'Limonada', tipo: 'Bebidas Frías', descripcion: 'Limonada fresca con hielo', precio: 5000 ,imagen: require('../../assets/images/limonada.png')},
+  { nombre: 'Coca-Cola', tipo: 'Bebidas Frías', descripcion: 'Refresco de cola', precio: 4500 ,imagen: require('../../assets/images/cocacola.jpg')},
+  { nombre: 'Té Helado', tipo: 'Bebidas Frías', descripcion: 'Té con limón y hielo', precio: 6000 ,imagen: require('../../assets/images/tehelado.png')},
+  { nombre: 'Agua Mineral', tipo: 'Bebidas Frías', descripcion: 'Agua con gas', precio: 3500 ,imagen: require('../../assets/images/aguamineral.png')},
+  { nombre: 'Sopa de Pollo', tipo: 'Sopas', descripcion: 'Sopa casera de pollo con fideos', precio: 12000 ,imagen: require('../../assets/images/sopadepollo.png')},
+  { nombre: 'Sopa de Tomate', tipo: 'Sopas', descripcion: 'Sopa cremosa de tomate', precio: 11000 ,imagen: require('../../assets/images/sopadetomate.png')},
+  { nombre: 'Sopa de Verduras', tipo: 'Sopas', descripcion: 'Sopa mixta de verduras', precio: 11500 ,imagen: require('../../assets/images/sopadeverduras.png')},
+  { nombre: 'Sopa de Mariscos', tipo: 'Sopas', descripcion: 'Sopa con una mezcla de mariscos', precio: 15000 ,imagen: require('../../assets/images/sopademariscos.png')},
+  { nombre: 'Pollo a la Parrilla', tipo: 'Platos del Día', descripcion: 'Pollo con arroz y ensalada', precio: 20000 ,imagen: require('../../assets/images/polloalaParrilla.png')},
+  { nombre: 'Carne Asada', tipo: 'Platos del Día', descripcion: 'Carne de res con papas fritas', precio: 22000 ,imagen: require('../../assets/images/carneAsada.png')},
+  { nombre: 'Pasta Alfredo', tipo: 'Platos a la Carta', descripcion: 'Pasta con salsa alfredo y pollo', precio: 18000 ,imagen: require('../../assets/images/pastaAlfredo.png')},
+  { nombre: 'Pizza Margarita', tipo: 'Platos a la Carta', descripcion: 'Pizza con tomate, mozzarella y albahaca', precio: 25000 ,imagen: require('../../assets/images/pizzaMargarita.png')},
+  { nombre: 'Hamburguesa', tipo: 'Platos a la Carta', descripcion: 'Hamburguesa con queso y papas fritas', precio: 17000 ,imagen: require('../../assets/images/hamburguesa.png')},
+  { nombre: 'Tacos', tipo: 'Platos a la Carta', descripcion: 'Tacos con carne, queso y guacamole', precio: 16000 ,imagen: require('../../assets/images/tacos.png')},
+  { nombre: 'Ensalada César', tipo: 'Platos a la Carta', descripcion: 'Ensalada con pollo y aderezo César', precio: 14000 ,imagen: require('../../assets/images/cesar.png')},
+  { nombre: 'Risotto de Champiñones', tipo: 'Platos a la Carta', descripcion: 'Risotto cremoso con champiñones', precio: 20000 ,imagen: require('../../assets/images/risotto.png')},
+  { nombre: 'Menú Infantil', tipo: 'Menú Infantil', descripcion: 'Nuggets de pollo con papas fritas', precio: 12000 ,imagen: require('../../assets/images/nuggets.png')},
 ];
 
 export default function HomeScreen({ navigation }) {
@@ -82,10 +81,6 @@ export default function HomeScreen({ navigation }) {
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.headerText}>Bienvenido a su restaurante a domicilio</Text>
-        <Image source={imgT} style={{
-    width: 50,
-    height: 50,
-  }} />
         <TouchableOpacity onPress={irAlCarrito} style={styles.cartIcon}>
           <Icon name="shopping-cart" size={30} color="#fff" />
         </TouchableOpacity>
@@ -127,6 +122,7 @@ export default function HomeScreen({ navigation }) {
         <ScrollView style={styles.menuContainer}>
           {menuItems.map((plato, index) => (
             <View key={index} style={styles.menuItem}>
+              <Image source={plato.imagen} style={{ width: 100, height: 100 }} /> {/* Imagen del plato */}
               <Text style={styles.menuItemTitle}>{plato.nombre}</Text>
               <Text style={styles.menuItemType}>{plato.tipo}</Text>
               <Text style={styles.menuItemDescription}>{plato.descripcion}</Text>
